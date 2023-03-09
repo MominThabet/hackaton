94e4427d529bada758538f6a2c1edb431ecbcf30
MominThabet <momin.thabet@gmail.com>
2023-03-09 13:17:39 +0200

    isAuth

--- src/utils/middleware/auth/auth.js
+++ src/utils/middleware/auth/auth.js
@@ -1,5 +1,5 @@
 const { verifyAccessToken } = require('../../jwt');
-const { AccessDenied, Unauthorized } = require('../../responses/error/errors');
+const { Unauthorized } = require('../../responses/error/errors');
 const User = require('../../../model/user');
 
 module.exports.isAuthenticated = async (req, res, next) => {
@@ -10,31 +10,22 @@ module.exports.isAuthenticated = async (req, res, next) => {
   if (!authorization.startsWith('Bearer')) {
     return next(new Unauthorized('Unauthorized'));
   }
-
   const split = authorization.split('Bearer ');
   if (split.length !== 2) {
     return next(new Unauthorized('Unauthorized'));
   }
   const token = split[1];
-
   try {
     const decodedToken = verifyAccessToken(token);
 
     if (!decodedToken) {
       return next(new Unauthorized('Unauthorized'));
     }
-
     const user = await User.findOne({ _id: decodedToken.data.id });
     if (!user) {
       return next(new Unauthorized('Unauthorized'));
     }
 
-    if (user.isDeleted) {
-      return next(new AccessDenied('Deleted Account'));
-    }
-    if (user.isBlocked) {
-      return next(new AccessDenied('Blocked Account'));
-    }
     req.user = user;
     return next();
   } catch (err) {
